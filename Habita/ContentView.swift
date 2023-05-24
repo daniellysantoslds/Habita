28442e7910e4dd87626504696e8d08af9a2bd659
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-24 16:37:48 -0300

    adding colors in assets folder

--- Habita/ContentView.swift
+++ Habita/ContentView.swift
@@ -14,7 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
-            Image("search")
+            Image("search").foregroundColor(.pink)
         }
         .padding()
     }
