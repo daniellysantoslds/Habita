2bfe15b599a4cfe5fc6f4a4d7df865b775fc54a6
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-24 17:31:02 -0300

    creating Componentes Folder

---
+++ Habita/Components/Buttons/BigNormalButton.swift
@@ -0,0 +1,20 @@
+//
+//  BigNormalButtonComponent.swift
+//  Habita
+//
+//  Created by Danielly Santos Lopes da Silva on 24/05/23.
+//
+
+import SwiftUI
+
+struct BigNormalButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct BigNormalButtonComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        BigNormalButton()
+    }
+}
