85f7be9f6fa62959c165737dc0a0f195186c079a
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-24 15:13:44 -0300

    first commit

---
+++ Habita/HabitaApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitaApp.swift
+//  Habita
+//
+//  Created by Danielly Santos Lopes da Silva on 24/05/23.
+//
+
+import SwiftUI
+
+@main
+struct HabitaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
