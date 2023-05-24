a15a9221f000ced339e32d5a6084819f1f0eab6d
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-24 18:02:13 -0300

    creating BigNormalButton Model and responsinees

--- Habita/Components/Buttons/BigNormalButton.swift
+++ Habita/Components/Buttons/BigNormalButton.swift
@@ -8,13 +8,44 @@
 import SwiftUI
 
 struct BigNormalButton: View {
+    
+    let titleButton : String;
+    
+    
+    init(titleButton: String) {
+        self.titleButton = titleButton
+    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            Spacer()
+            
+            GeometryReader { geometry in
+                
+                Button {
+                    
+                } label: {
+                    Text(titleButton)
+                        .foregroundColor(Color("White-0"))
+                        .fontWeight(.medium)
+                        .font(.system(size: 15))
+                        .frame(width: UIScreen.main.bounds.size.width * 0.91, height: UIScreen.main.bounds.size.height * 0.08)
+                        .background(Color("Primary-0"))
+                        .cornerRadius(4)
+                }
+                
+            }
+            
+            Spacer()
+        }
+        
     }
 }
 
 struct BigNormalButtonComponent_Previews: PreviewProvider {
     static var previews: some View {
-        BigNormalButton()
+        BigNormalButton(titleButton: "Normal")
     }
 }
