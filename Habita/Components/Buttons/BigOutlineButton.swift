a118e91dd3a0fc958228134432b32f889dfa54da
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-25 15:36:54 -0300

    adding BiOutlineButton in Buttons Folder

---
+++ Habita/Components/Buttons/BigOutlineButton.swift
@@ -0,0 +1,56 @@
+//
+//  BigOutlineButton.swift
+//  Habita
+//
+//  Created by Danielly Santos Lopes da Silva on 25/05/23.
+//
+
+import SwiftUI
+
+struct BigOutlineButton: View {
+    
+    let titleButton: String;
+    @State private var isHovered = false
+    
+    init(titleButton: String) {
+        self.titleButton = titleButton
+    }
+    
+    
+    var body: some View {
+        
+        
+        GeometryReader { geometry in
+            Button {
+                print("printou")
+            } label: {
+                Text(titleButton)
+                    .foregroundColor(Color("Primary-0"))
+                    .fontWeight(.medium)
+                    .font(.system(size: 15))
+                    .frame(width: UIScreen.main.bounds.width * 0.91, height: UIScreen.main.bounds.height * 0.08)
+                    .background(Color("White-0"))
+                    .overlay(
+                        RoundedRectangle(cornerRadius: 4)
+                            .stroke(isHovered ?
+                                    Color("Primary+1") :
+                                        Color("Primary-0"), lineWidth: 1))
+                    .onTapGesture {
+                        withAnimation(.easeOut(duration: 0.2)) {
+                            isHovered.toggle()
+                        }
+                    }
+            }
+        }
+        
+        
+        
+        
+    }
+}
+
+struct BigOutlineButton_Previews: PreviewProvider {
+    static var previews: some View {
+        BigOutlineButton(titleButton: "Outline")
+    }
+}
