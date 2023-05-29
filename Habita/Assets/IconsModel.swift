fca1b411c982c54e02341c4db45edd91a162cd4f
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-05-29 14:11:15 -0300

    creating Assets Folder

---
+++ Habita/Assets/IconsModel.swift
@@ -0,0 +1,20 @@
+//
+//  IconsView.swift
+//  Habita
+//
+//  Created by Danielly Santos Lopes da Silva on 29/05/23.
+//
+
+import SwiftUI
+
+extension Image {
+    static func myAppIcon1() -> Image {
+        Image(systemName: "xmark")
+    }
+    
+    static func myAppIcon2() -> Image {
+        Image(systemName: "iconName2")
+    }
+    
+    // Adicione mais métodos estáticos para cada ícone do seu aplicativo
+}
