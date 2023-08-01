ca75631a50b5dd5d0db7a2d546b5ac532951f8c1
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-08-01 17:44:32 -0300

    hotfix:add changeds

--- Habita/View/SearchView.swift
+++ Habita/View/SearchView.swift
@@ -9,58 +9,61 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        NavigationView {
+        
+        VStack {
             VStack {
-                VStack {
-                    SearchInput()
-                    
-                }
-                .padding(.trailing, 16)
-                .frame(height: 80)
-                
-                
-                ScrollView {
-                    
-                    VStack(alignment: .leading, spacing: 24) {
-                        
-                        
-                        NavigationLink {
-                            SelectdImovelView()
-                        } label: {
-                            BigMainCard(imageProperty: "imov2", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                        }
-                        
-                        
-                        Spacer(minLength: 340)
-                        
-                        BigMainCard(imageProperty: "imov1", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                        
-                        Spacer(minLength: 340)
-                        
-                        BigMainCard(imageProperty: "imov3", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                        
-                        Spacer(minLength: 340)
-                        
-                        BigMainCard(imageProperty: "imov4", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                        
-                        Spacer(minLength: 340)
-                        
-                        BigMainCard(imageProperty: "imov5", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                        
-                        Spacer(minLength: 340)
-                        
-                        
-                        
-                    }
-                    
-                }
-                
-                .padding(.leading, 16)
-                //.padding(.trailing, -16)
-                
+                SearchInput()
                 
             }
+            .padding(.trailing, 16)
+            .frame(height: 80)
+            
+            
+            ScrollView {
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    
+                    Button(action: {
+                        
+                        print("oi")
+                    }) {
+                        
+                        NavigationLink(destination: SelectdImovelView()) {
+                            BigMainCard(imageProperty: "imov2", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                        }
+                    }
+                    
+                    
+                    Spacer(minLength: 340)
+                    
+                    BigMainCard(imageProperty: "imov1", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                    
+                    Spacer(minLength: 340)
+                    
+                    BigMainCard(imageProperty: "imov3", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                    
+                    Spacer(minLength: 340)
+                    
+                    BigMainCard(imageProperty: "imov4", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                    
+                    Spacer(minLength: 340)
+                    
+                    BigMainCard(imageProperty: "imov5", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                    
+                    Spacer(minLength: 340)
+                    
+                    
+                    
+                }
+                
+            }
+            
+            .padding(.leading, 16)
+            //.padding(.trailing, -16)
+            
+            
         }
+        
         
         
     }
--- Habita/View/SelectdImovelView.swift
+++ Habita/View/SelectdImovelView.swift
@@ -13,13 +13,13 @@ struct SelectdImovelView: View {
             Text("oi")
             PropertyCode(codeID: "COD. 299")
             BigListOfValuesCard(rentText: "Aluguel", condominiumText: "Condomínio", iptuText: "IPTU", totalText: "Total", rentValue: "R$1.200", condominiumValue: "R$ 600", iptuValue: "R$ 300", totalValue: "R$ 2.100")
-          
+            
             AmentiesCard(roomIcon: Image.bedIcon(), bathroomIcon: Image.bathhubIcon(), spaceIcon: Image.helpIcon(), heightIcon: Image.triangleIcon(), roomDescription: "quartos", amountRoomDescription: "2", bathroomDescription: "banheiro", amountBathroomDescription: "1", spaceDescription: "m²", amountSpaceDescription: "60", heightDescription: "andar", amountHeightDescription: "3º")
             
         }
         .padding(.leading, 16)
         .padding(.trailing, 16)
-      
+        
     }
 }
 
