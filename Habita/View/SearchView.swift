002b847a9f876a8b6c29568adf37d4af33b9d7f1
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-08-01 17:06:27 -0300

    hotfix: addding spacings in views

--- Habita/View/SearchView.swift
+++ Habita/View/SearchView.swift
@@ -9,47 +9,57 @@ import SwiftUI
 
 struct SearchView: View {
     var body: some View {
-        VStack {
+        NavigationView {
             VStack {
-                SearchInput()
+                VStack {
+                    SearchInput()
+                    
+                }
+                .padding(.trailing, 16)
+                .frame(height: 80)
                 
-            }
-            .padding(.trailing, 16)
-            .frame(height: 80)
-            
-            
-            ScrollView {
                 
-                VStack(alignment: .leading, spacing: 24) {
+                ScrollView {
                     
-                    BigMainCard(imageProperty: "imov2", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                    
-                    Spacer(minLength: 340)
-                    
-                    BigMainCard(imageProperty: "imov1", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                    
-                    Spacer(minLength: 340)
-                    
-                    BigMainCard(imageProperty: "imov3", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                    
-                    Spacer(minLength: 340)
-                    
-                    BigMainCard(imageProperty: "imov4", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                    
-                    Spacer(minLength: 340)
-                    
-                    BigMainCard(imageProperty: "imov5", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
-                    
-                    Spacer(minLength: 340)
-                    
-                    
+                    VStack(alignment: .leading, spacing: 24) {
+                        
+                        
+                        NavigationLink {
+                            SelectdImovelView()
+                        } label: {
+                            BigMainCard(imageProperty: "imov2", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                        }
+                        
+                        
+                        Spacer(minLength: 340)
+                        
+                        BigMainCard(imageProperty: "imov1", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                        
+                        Spacer(minLength: 340)
+                        
+                        BigMainCard(imageProperty: "imov3", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                        
+                        Spacer(minLength: 340)
+                        
+                        BigMainCard(imageProperty: "imov4", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                        
+                        Spacer(minLength: 340)
+                        
+                        BigMainCard(imageProperty: "imov5", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes", rentDescription: "Aluguel R$ 1.500", totalValueDescription: "Total R$ 2.000")
+                        
+                        Spacer(minLength: 340)
+                        
+                        
+                        
+                    }
                     
                 }
                 
+                .padding(.leading, 16)
+                //.padding(.trailing, -16)
+                
+                
             }
-            
-            .padding(.leading, 16)
-            
         }
         
         
