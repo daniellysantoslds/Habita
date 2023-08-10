231a7165438c47efc9cd8789b0f178e02fc8cf44
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-08-10 11:50:36 -0300

    updates

--- Habita/ContentView.swift
+++ Habita/ContentView.swift
@@ -12,6 +12,7 @@ struct ContentView: View {
         VStack {
 //            SmallProposalsCard(dateProposal: "Quar., 22/04", imageProperty: Image("imov2"), codeID: "Imóvel  Cod. 299", addressDescription: "Rua glicerio 478, Graças", spaceDescription: "270m", roomDescription: "2 quartos", suitesDescription: "2 suítes")
 //
+            
         }
         
     }
@@ -19,6 +20,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-       ContentView() 
+       CardTest() 
     }
 }
