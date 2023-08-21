f9f2c0a00c1f4812d7b9399df806834ca4161769
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-08-21 08:50:49 -0300

    hotfix:adding components in SelectImovelView

--- Habita/View/SelectImovelView.swift
+++ Habita/View/SelectImovelView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct SelectImovelView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        GeometryReader { geometry in
+            
+            
+            VStack {
+                
+                //creating variables the texts 
+                
+                Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+                
+                BigListOfValuesCard(rentText: "Aluguel", condominiumText: "Condomínio", iptuText: "IPTU", totalText: "Total", rentValue: "R$1.200", condominiumValue: "R$ 600", iptuValue: "R$ 300", totalValue: "R$ 2.100")
+                
+                
+                AmentiesCard(roomIcon: Image.bedIcon(), bathroomIcon: Image.bathhubIcon(), spaceIcon: Image.helpIcon(), heightIcon: Image.triangleIcon(), roomDescription: "quartos", amountRoomDescription: "2", bathroomDescription: "banheiro", amountBathroomDescription: "1", spaceDescription: "m²", amountSpaceDescription: "60", heightDescription: "andar", amountHeightDescription: "3º")
+                
+                
+                
+                
+            }
+        }
+        
     }
 }
 
