0bf3ab97a76aa3b3a83924a4ac22cf8661ac1001
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-09-04 15:26:07 -0300

    hotfix: updates in interface

--- Habita/View/SelectdImovelView.swift
+++ Habita/View/SelectdImovelView.swift
@@ -29,20 +29,36 @@ struct SelectdImovelView: View {
         
         ScrollView {
             VStack(alignment: .leading) {
-                Text("oi")
+                
+                VStack(alignment: .leading, spacing: 8) {
+                    Text("Apartamento  com 2 quartos, 60 m²")
+                        .font(.system(size: 18, weight: .regular))
+                    
+                    Text("Rua glicerio 478, Graças")
+                        .font(.system(size: 15, weight: .regular))
+                }
+                
+                
                 PropertyCode(codeID: "COD. 299")
+                
+                .padding(.top, 8)
+                .padding(.bottom, 24)
                 BigListOfValuesCard(rentText: "Aluguel", condominiumText: "Condomínio", iptuText: "IPTU", totalText: "Total", rentValue: "R$1.200", condominiumValue: "R$ 600", iptuValue: "R$ 300", totalValue: "R$ 2.100")
                 
+                .padding(.bottom, 24)
                 AmentiesCard(roomIcon: Image.bedIcon(), bathroomIcon: Image.bathhubIcon(), spaceIcon: Image.helpIcon(), heightIcon: Image.triangleIcon(), roomDescription: "quartos", amountRoomDescription: "2", bathroomDescription: "banheiro", amountBathroomDescription: "1", spaceDescription: "m²", amountSpaceDescription: "60", heightDescription: "andar", amountHeightDescription: "3º")
                 
-                
+                    .padding(.bottom, 24)
                 VStack(alignment: .leading, spacing: 16) {
                     Text("Descrição")
                         .font(.system(size: 15, weight: .medium))
                     Text(immobileDescription)
                         .font(.system(size: 12, weight: .regular))
                 }
+                
                 Divider()
+                
+                    .padding(.bottom, 24)
                 
                 VStack(alignment: .leading, spacing: 16) {
                     
