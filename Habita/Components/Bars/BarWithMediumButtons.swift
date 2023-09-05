d9618baedf459e551a43b18d91f64d1d7e7ddd60
Danielly Lopes <102704880+daniellysantoslds@users.noreply.github.com>
2023-09-05 14:05:59 -0300

    feature:creating BarWithMediumButtons

---
+++ Habita/Components/Bars/BarWithMediumButtons.swift
@@ -0,0 +1,24 @@
+//
+//  BarWithLargueButtons.swift
+//  Habita
+//
+//  Created by Danielly Santos Lopes da Silva on 05/09/23.
+//
+
+import SwiftUI
+
+struct BarWithMediumButtons: View {
+    var body: some View {
+        
+        HStack {
+            Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        }
+       
+    }
+}
+
+struct BarWithMediumButtons_Previews: PreviewProvider {
+    static var previews: some View {
+        BarWithMediumButtons()
+    }
+}
